2f107aa8079ef269dc296759a1e6db905b167e9a
Cory Meyers and Michael Dada <whippoorwill.in.flight@gmail.com>
2016-01-25 15:30:39 -0800

    add working task list populator

--- todolist/js/scripts.js
+++ todolist/js/scripts.js
@@ -5,6 +5,23 @@ function toDoList(task, priority, deadline) {
   this.entire_task = [];
 }
 
-toDoList.prototype.entire_task = function(){
+toDoList.prototype.entireTask = function() {
   return this.task + ", " + this.priority + ", " + this.deadline;
 }
+
+$(document).ready(function() {
+  $('form#taskForm').submit(function(event) {
+    event.preventDefault();
+
+    var newTask = $("input#new-task").val();
+    var newPriority = $("input#new-priority").val();
+    var newDeadline = $("input#new-deadline").val();
+    var newTask = new toDoList(newTask, newPriority, newDeadline);
+
+
+    $("ul#tasklist").append("<li><span class='taskInsert'>" + newTask.entireTask() + "</span></li>");
+
+
+
+  });
+});

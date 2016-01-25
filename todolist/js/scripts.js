10de31af0bc61bca1c98b655ecc08b30571f7ffb
Cory Meyers and Michael Dada <mail.michaeldada@gmail.com>
2016-01-25 14:19:24 -0800

    add script to take task properties and combine them into a new propery called entire_task

--- todolist/js/scripts.js
+++ todolist/js/scripts.js
@@ -1,3 +1,10 @@
-function toDoList(task) {
-  this.task = task
+function toDoList(task, priority, deadline) {
+  this.task = task;
+  this.priority = priority;
+  this.deadline = deadline;
+  this.entire_task = [];
+}
+
+toDoList.prototype.entire_task = function(){
+  return this.task + ", " + this.priority + ", " + this.deadline;
 }
